68e1939234e6ba669946058def3a251d1079f1fd
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2022-10-08 12:56:15 +0200

    Finish Minecraft proxy + DynamicProcess

--- modules/mc-proxy.js
+++ modules/mc-proxy.js
@@ -1,15 +1,23 @@
 const util = require("util");
 const proxy = require("node-tcp-proxy");
+const DynamicProcess = require("../lib/dynamic-process");
+const mcProcess = new DynamicProcess({
+  command: "java",
+  cwd: "D:\\MCServer",
+  args: ["-jar", "D:\\MCServer\\server.jar", "nogui"],
+});
+
 const mcProxy = proxy.createProxy(25565, "localhost", 25566, {
   upstream: function (ctx, data) {
-    console.log(
+    /*console.log(
       util.format(
         "Client %s:%s sent:",
         ctx.proxySocket.remoteAddress,
         ctx.proxySocket.remotePort
       )
     );
-    console.log(data.toString("utf-8"));
+    console.log(data.toString("utf-8"));*/
+    mcProcess.poke();
 
     return data;
   },
