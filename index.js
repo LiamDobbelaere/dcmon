74f5fb42ce79aff4f0d5f97eb3512e6be1ca93a9
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2022-10-13 21:10:04 +0200

    Add fastdl support

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const useModules = [["server-info", "/serverinfo"], ["mc-proxy"]];
 
+app.use(express.static(__dirname + "/public"));
+
 useModules.forEach(([moduleName, mountPoint]) => {
   const module = require(`./modules/${moduleName}.js`);
   if (mountPoint) {
     app.use(mountPoint, module);
+    console.log(`[Init] ${moduleName} mounted at ${mountPoint}`);
+  } else {
+    console.log(`[Init] ${moduleName} loaded (no mount specified)`);
   }
 });
 
 app.listen(80, () => {
-  console.log("Running!");
+  console.log("[Init] HTTP ready!");
 });
--- modules/mc-proxy.js
+++ modules/mc-proxy.js
@@ -10,18 +10,7 @@ const mcProcess = new DynamicProcess({
 
 const mcProxy = proxy.createProxy(25565, "localhost", 25566, {
   upstream: function (ctx, data) {
-    /*console.log(
-      util.format(
-        "Client %s:%s sent:",
-        ctx.proxySocket.remoteAddress,
-        ctx.proxySocket.remotePort
-      )
-    );
-    console.log(data.toString("utf-8"));*/
     mcProcess.poke();
-
     return data;
   },
 });
-
-console.log("MCProxy running!");
